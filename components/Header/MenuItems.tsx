9de4a393bd7b1f8bddbb42c9c9a42b89fffee02e
tohsaka888 <156132264@qq.com>
2022-08-27 10:29:06 +0800

    ✨ feat: menu配置

---
+++ components/Header/MenuItems.tsx
@@ -0,0 +1,6 @@
+import { BsTrophyFill, BsTwitter } from "react-icons/bs";
+
+export const MenuItems = [
+  { label: '竞赛 / Competition', key: 'competition', icon: <BsTrophyFill size={13} /> }, // 菜单项务必填写 key
+  { label: '活动 / Activity', key: 'activity', icon: <BsTwitter /> },
+]
