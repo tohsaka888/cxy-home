15ac0d22d66d50fd7f04900b98cebdfe3c5594ae
tohsaka888 <156132264@qq.com>
2022-08-27 10:29:35 +0800

    ✨ feat: HomePage

---
+++ components/HomePage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { CoderName, CoderNameEn, Container } from './index.styled'
+
+function HomePage() {
+  return (
+    <Container>
+      <CoderName>程序员之家</CoderName>
+      <CoderNameEn>The Home Of Coder </CoderNameEn>
+    </Container>
+  )
+}
+
+export default HomePage
