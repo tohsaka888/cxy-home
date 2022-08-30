b4601bb54d5a10134e8f6b547df2970383faa618
tohsaka888 <156132264@qq.com>
2022-08-30 15:22:39 +0800

    ✨ feat: 参与者表格

---
+++ components/Competition/ParticipantsTable.tsx
@@ -0,0 +1,32 @@
+import { Table } from 'antd'
+import React from 'react'
+
+type Props = {
+  competition: Competition.Competition
+}
+
+const columns = [
+  {
+    title: '序号',
+    dataIndex: 'index',
+    render: (text: string, record: any, index: number) => index + 1
+  },
+  {
+    title: '用户名',
+    dataIndex: 'username',
+    key: 'username',
+  },
+  {
+    title: '邮箱',
+    dataIndex: 'email',
+    key: 'email',
+  },
+];
+
+function ParticipantsTable({ competition }: Props) {
+  return (
+    <Table dataSource={competition.participants} columns={columns} pagination={{ pageSize: 5, showQuickJumper: true }} />
+  )
+}
+
+export default ParticipantsTable
