9128bfc97f76b822fb23708fed56365ee1df0b32
tohsaka888 <156132264@qq.com>
2022-08-30 15:25:21 +0800

    🦄 refactor: update type

--- typings/competition.d.ts
+++ typings/competition.d.ts
@@ -3,4 +3,49 @@ declare namespace Competition {
     id: string;
     name: string;
   }
+  type Participant = {
+    username: string;
+    email: string;
+  }
+
+  type CompetitionImage = {
+    intro: string;
+    url: string;
+  }
+
+  type Award = {
+    award: string;
+  }
+
+  type AwardSetting = {
+    award: string;
+    limit: number;
+  }
+
+  type Winner = Participant & Award;
+
+  type Creator = Participant;
+
+  type Info = {
+    place: string;
+    way: string;
+    limit: number;
+    signUpStart: string;
+    signUpEnd: string;
+    time: string;
+    duration: string;
+  }
+
+  type Competition = {
+    name: string;
+    createdTime: string;
+    updatedTime: string;
+    participants: Participant[];
+    winners: Winner[];
+    info: Info;
+    intro: string;
+    banners: CompetitionImage[];
+    creator: Creator;
+    awardSetting: AwardSetting[];
+  }
 }
