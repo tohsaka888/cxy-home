9766e7251250ace0ff4e820f7214dbac6ac5fea0
tohsaka888 <156132264@qq.com>
2022-08-30 15:20:24 +0800

    ✨ feat(config): 配置图片域名

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import transpile from "next-transpile-modules";
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  images: {
+    domains: ['42.200.211.25']
+  },
   compiler: {
     styledComponents: true,
   },
