6ac73759fcf77bd46eea249f55855f14c6d3f8eb
tohsaka888 <156132264@qq.com>
2022-08-30 15:21:44 +0800

    🌈 style: 比赛模块样式

---
+++ components/Competition/index.styled.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #fff;
+  padding: 8px 16px;
+  margin-bottom: 16px;
+  box-shadow: 0px 0px 10px 5px #dfdfdf;
+`
+
+export const CompetitionName = styled.h1`
+  font-size: 1.6rem;
+  font-weight: bold;
+  border-bottom: 1px solid #cecece;
+  padding-bottom: 5px;
+`
+
+export const Username = styled.div`
+  font-weight: bold;
+`
+
+export const Email = styled.a`
+  font-weight: bold;
+  text-decoration: none;
+  font-size: 12px;
+`
+
+export const TimeLabel = styled.div`
+  font-weight: bold;
+  margin-right: 8px;
+`
+
+export const Title = styled.div`
+  padding: 8px;
+  font-size: 14px;
+  font-weight: bold;
+  border-bottom: 1px solid #cecece;
+`
