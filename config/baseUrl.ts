ddf91464b13268106562b1ee72dde8e25f8884e4
tohsaka888 <156132264@qq.com>
2022-08-30 08:32:23 +0800

    ✨ feat(config): 增加竞赛api主路由

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -1,2 +1,3 @@
 export const loginUrl = `https://cxy-home-login.vercel.app`
 export const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://cxy-home.vercel.app'
+export const competitionUrl = `https://cxy-home-competition.vercel.app`
