8df91345c9f19d7675530461a9970bd349f7f961
tohsaka888 <156132264@qq.com>
2022-08-30 17:26:28 +0800

    🌈 style: update style

--- components/Competition/index.styled.ts
+++ components/Competition/index.styled.ts
@@ -7,11 +7,10 @@ export const Container = styled.div`
   box-shadow: 0px 0px 10px 5px #dfdfdf;
 `
 
-export const CompetitionName = styled.h1`
+export const CompetitionName = styled.div`
   font-size: 1.6rem;
   font-weight: bold;
-  border-bottom: 1px solid #cecece;
-  padding-bottom: 5px;
+  /* padding-bottom: 5px; */
 `
 
 export const Username = styled.div`
@@ -34,4 +33,30 @@ export const Title = styled.div`
   font-size: 14px;
   font-weight: bold;
   border-bottom: 1px solid #cecece;
+`
+
+export const InfoText = styled.a`
+  font-size: 1.3rem;
+  font-weight: bold;
+  font-family: Segoe UI;
+  text-shadow: 2px 2px 3px #cecece;
+`
+
+export const InfoLabel = styled.div`
+  /* font-family: '楷体'; */
+  /* font-weight: bold; */
+  font-size: 14px;
+  border-bottom: 1px solid #cecece;
+  padding-bottom: 3px;
+`
+
+export const InfoContainer = styled.div`
+  flex: 1;
+  padding: 8px 8px;
+  background: #f9f9f9;
+  margin-top: 6px;
+  margin-right: 6px;
+  border-radius: 5px;
+  border: 1px solid #cecece;
+  margin-bottom: 8px;
 `
