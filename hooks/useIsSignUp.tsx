e8bd70b0844629a3558e8f614cee5a45424d181f
tohsaka888 <156132264@qq.com>
2022-09-01 09:22:23 +0800

    🎈 perf: 优化hooks

--- hooks/useIsSignUp.tsx
+++ hooks/useIsSignUp.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import useSWR from 'swr'
 
 function useIsSignUp(id: string, username?: string) {
-  const { data: isSignUp } = useSWR<boolean>(username ? `${competitionUrl}/api/competition/is-sign-up/${id}` : null, async (url) => {
+  const { data: isSignUp, error } = useSWR<boolean>(username ? `${competitionUrl}/api/competition/is-sign-up/${id}` : null, async (url) => {
     const res = await fetch(url, {
       method: 'POST',
       headers: {
@@ -14,9 +14,9 @@ function useIsSignUp(id: string, username?: string) {
     })
     const data = await res.json()
     return data.isSignUp
-  })
+  }, {refreshInterval: 100})
 
-  return isSignUp
+  return { isSignUp, loading: isSignUp === undefined }
 }
 
 export default useIsSignUp
