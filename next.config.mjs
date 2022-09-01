699cf6f49f8528f8fce10719dc22199c4f67f97f
tohsaka888 <156132264@qq.com>
2022-09-01 11:22:07 +0800

    🧪 test: 测试服务端渲染

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import transpile from "next-transpile-modules";
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  staticPageGenerationTimeout: 60000,
   images: {
     domains: ['42.200.211.25']
   },
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import HomePage from '@components/HomePage'
 import { competitionUrl } from '@config/baseUrl'
 import { ListContext } from 'context/listContext'
+import createFetch from '@vercel/fetch'
 import type { GetServerSideProps, NextPage } from 'next'
 import dynamic from 'next/dynamic'
+import Fetch from 'node-fetch'
 
 const StarBackground = dynamic(() => import('@components/StarBackground'), { ssr: false })
 
@@ -17,17 +19,29 @@ const Home: NextPage<{ list: Competition.List[] }> = ({ list }) => {
   )
 }
 
-// export const getServerSideProps: GetServerSideProps = async () => {
-//   const res = await fetch(`${competitionUrl}/api/brief`, {
-//     mode: 'cors'
-//   })
-//   const data = await res.json()
-//   console.log(data)
-//   return {
-//     props: {
-//       list: data.list
-//     }
-//   }
-// }
+export const getServerSideProps: GetServerSideProps = async () => {
+  // const myfetch = createFetch(undefined, {
+  //   timeout: 60000,
+  //   keepAliveMsecs: 6000,
+  //   maxSockets: 50000
+  // })
+  let data = null
+  try {
+    const res = await fetch(`${competitionUrl}/api/brief`, {
+      headers: {
+        'Content-Type': ' application/json'
+      }
+    })
+    data = await res.json()
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+  return {
+    props: {
+      list: data ? data.list : []
+    }
+  }
+}
 
 export default Home
