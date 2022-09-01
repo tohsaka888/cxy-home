275cd23f8006e6edceb22cc14773fdcdb74d8767
tohsaka888 <156132264@qq.com>
2022-09-01 15:21:26 +0800

    ✨ feat(config): 配置dev环境baseUrl

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -1,3 +1,3 @@
 export const loginUrl = `https://cxy-home-login.vercel.app`
-export const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://cxy-home.vercel.app'
-export const competitionUrl = `https://cxy-home-competition.vercel.app`
+export const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://cxy-home.vercel.app'
+export const competitionUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : `https://cxy-home-competition.vercel.app`
