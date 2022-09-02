db8c06877e49b6d76bca2de1e4f3d56f662176a8
tohsaka888 <156132264@qq.com>
2022-09-02 17:12:27 +0800

    ✨ feat: use @vercel/fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-09-02 14:10:53
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-09-02 17:12:03
+ * @Description: 请填写简介
+ */
 import Header from '@components/Header'
 import HomePage from '@components/HomePage'
 import { competitionUrl } from '@config/baseUrl'
 import type { GetServerSideProps, NextPage } from 'next'
 import dynamic from 'next/dynamic'
 import { SWRConfig } from 'swr'
+import createFetch from '@vercel/fetch'
 
 const StarBackground = dynamic(() => import('@components/StarBackground'), { ssr: false })
 
@@ -20,14 +28,10 @@ const Home: NextPage<{ fallback: any }> = ({ fallback }) => {
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  // const myfetch = createFetch(undefined, {
-  //   timeout: 60000,
-  //   keepAliveMsecs: 6000,
-  //   maxSockets: 50000
-  // })
+  const myfetch = createFetch()
   let data = null
   try {
-    const res = await fetch(`${competitionUrl}/api/brief`, {
+    const res = await myfetch(`${competitionUrl}/api/brief`, {
       headers: {
         'Content-Type': ' application/json'
       }
