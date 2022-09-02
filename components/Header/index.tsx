6584c57a670a18ee8924012ac1458a36c7090906
tohsaka888 <156132264@qq.com>
2022-09-02 17:11:34 +0800

    ✨ feat: 根据路由匹配选择项目

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,3 +1,10 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-09-02 14:10:53
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-09-02 17:04:20
+ * @Description: 请填写简介
+ */
 import React, { useCallback, useContext, useEffect, useMemo, useState } from 'react'
 import { ButtonArea, Container, Flex, Logo, Username } from './index.styled'
 import { Button, Menu, message } from 'antd'
@@ -16,17 +23,15 @@ function Header() {
   const router = useRouter()
   const [visible, setVisible] = useState<boolean>(false)
   // const { list } = useContext(ListContext)!
+  // console.log(router.pathname.split('/'))
   const isHomePage = useMemo(() => {
-    return !router.pathname.includes('competition')
+    return !router.pathname.includes('competition') && !router.pathname.includes('activity')
   }, [router])
 
   // const { setList } = useContext(ListContext)!
   const username = useLoginStatus()
 
   const { data, error } = useSWR(`${competitionUrl}/api/brief`, fetcher)
-
-
-  const list = useMemo(() => data && data.list, [data])
 
   return (
     <LoginContext.Provider value={{ visible, setVisible }}>
@@ -36,6 +41,7 @@ function Header() {
           <Menu
             items={MenuItems}
             mode={'horizontal'}
+            defaultSelectedKeys={[router.pathname.split('/')[1]]}
             style={{
               border: 'none',
               marginLeft: '16px',
@@ -44,7 +50,11 @@ function Header() {
               color: '#fff'
             }}
             onClick={(prop) => {
-              router.push('/' + prop.key + '/' + (list && list[0].id))
+              if (prop.key === 'competition') {
+                router.push('/' + prop.key + '/' + (data.list && data.list[0].id))
+              } else {
+                router.push('/' + prop.key)
+              }
             }}
           />
         </Flex>
@@ -71,9 +81,9 @@ function Header() {
                 }
               }} />}
           <BsGithub size={28} style={{ marginRight: '16px', color: '#fff' }} />
-          {username 
-          ? <Button type="primary" shape='round' onClick={() => { localStorage.removeItem('token') }} danger>退出登录</Button>
-          : <Button type="primary" shape='round' onClick={() => { setVisible(true) }}>登录</Button>}
+          {username
+            ? <Button type="primary" shape='round' onClick={() => { localStorage.removeItem('token') }} danger>退出登录</Button>
+            : <Button type="primary" shape='round' onClick={() => { setVisible(true) }}>登录</Button>}
         </ButtonArea>
       </Container >
       <LoginPanel />
