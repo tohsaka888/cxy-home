952e0628c422b28e958424553641d232e224856e
tohsaka888 <156132264@qq.com>
2022-09-02 17:11:49 +0800

    ✨ feat: 配置全局默认头像

---
+++ config/global.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-09-02 16:18:46
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-09-02 16:18:47
+ * @Description: 请填写简介
+ */
+
+import global_avatar from 'assets/images/avatar.jpg'
+
+export const avatarUrl = global_avatar
