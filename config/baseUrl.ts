cf9088979ce52c5c2c384ce3fa60c9c0ebc5181c
tohsaka888 <156132264@qq.com>
2022-09-09 17:08:07 +0800

    ✨ feat: use netlify

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -1,4 +1,11 @@
-export const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://cxy-home.vercel.app'
-export const activityUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://cxy-home-activity.vercel.app'
-export const competitionUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3002' : `https://cxy-home-competition.vercel.app`
-export const loginUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3003' : `https://cxy-home-login.vercel.app`
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-09-02 14:10:53
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-09-09 17:07:43
+ * @Description: 请填写简介
+ */
+export const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://cxy-home.netlify.app'
+export const activityUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://cxy-home-activity.netlify.app'
+export const competitionUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3002' : `https://cxy-home-competition.netlify.app`
+export const loginUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3003' : `https://cxy-home-login.netlify.app`
