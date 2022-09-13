d85aa05cc58b1625d5017f4041554c739f73b616
tohsaka888 <156132264@qq.com>
2022-09-13 17:03:36 +0800

    📃 docs: add doc

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -2,8 +2,8 @@
  * @Author: tohsaka888
  * @Date: 2022-09-02 14:10:53
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-09-09 17:07:43
- * @Description: 请填写简介
+ * @LastEditTime: 2022-09-13 17:03:20
+ * @Description: 主路由配置
  */
 export const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://cxy-home.netlify.app'
 export const activityUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://cxy-home-activity.netlify.app'
